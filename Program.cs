55abf9fd73173457ae191a57b18afe7329cf95bc
Christian Doyle <doyaldinho@gmail.com>
2021-05-02 15:16:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = .75;
+            Guy.Guy player = new Guy.Guy { cash = 100, name = "The Player" };
+
+            Console.WriteLine("Welcom to the Casino. The odds are " + odds);
+            while (player.cash > 0)
+            {
+                player.WriteMyInfo();
+
+                Console.WriteLine("How much would you like to bet?");
+                string howMuch = Console.ReadLine();
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;
+                    if (pot > 0)
+                    {
+                        if (random.NextDouble() > odds)
+                        {
+                            int winnings = pot;
+                            Console.WriteLine("You win " + winnings + " bucks!");
+                            player.ReceiveCash(winnings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bad luck, you lose.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid number");
+                    }
+                }
+            }
+            Console.WriteLine("The house always wins.");
+
+        }
+    }
+}
